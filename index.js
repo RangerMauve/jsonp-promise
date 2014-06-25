3948a9410acad8d213a7f117cb2250b4d8d0dea9
RangerMauve <RangerMauve@hotmail.com>
2014-06-25 16:04:59 -0400

    Fixed reference to wrong arguments instance

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var jsonp = require("jsonp");
 var Promise = require("promise");
 
 module.exports = function jsonp_promise(url, options) {
+	var args = [].slice.call(arguments);
 	return new Promise(function (resolve, reject) {
-		var args = [].slice.call(arguments);
 		args.push(function (err, res) {
 			if (err) reject(err)
 			else resolve(res);
